c950a75f4f8cf3c0a762cd5fb1ddc3f1c2224b03
DaniyalAli7778 <daniyal.personel7778@gmail.com>
2024-11-26 20:09:13 +0500

    "add one more folder "

--- bulb.js
+++
@@ -1,8 +0,0 @@
-var bulb = document.querySelector("#bulb")
-
-var btn = document.getElementById("button").addEventListener('click',print)
-
-let print= function(){
-  console.log("hellwo World");
-  
-};
